e38f6e7c41929a18d30de960c8f3c41a7941e884
Geoffrey-Anto <geoffreyanto.ignatiuse2021@vitstudent.ac.in>
2023-10-10 20:52:21 +0530

    ADDED: grpc impl for logger

--- logger/main.go
+++ logger/main.go
@@ -1,1 +1,70 @@
-package logger
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/geoffrey-anto/golang-microservice-apis/protos"
+)
+
+var (
+	port          = flag.Int("port", 50051, "The server port")
+	file *os.File = nil
+)
+
+type server struct {
+	pb.UnimplementedLoggerServer
+}
+
+func (s *server) SaveLog(ctx context.Context, in *pb.LogSaveRequest) (*pb.LogSaveRespone, error) {
+	fmt.Printf("%+v     %+v     %+v\n", in.Id, in.Log, in.Time)
+	if file == nil {
+		return &pb.LogSaveRespone{
+			Success: false,
+		}, fmt.Errorf("No file found")
+	}
+
+	_, err := file.WriteString(fmt.Sprintf("%+v     %+v     %+v\n", in.Id, in.Log, in.Time))
+	if err != nil {
+		return &pb.LogSaveRespone{
+			Success: false,
+		}, fmt.Errorf("Error in running file")
+	}
+
+	return &pb.LogSaveRespone{
+		Success: true,
+	}, nil
+}
+
+func main() {
+	flag.Parse()
+
+	f, err := os.Create("./logs/logs.txt")
+	file = f
+
+	if err != nil {
+		log.Fatalf("Error in creating file")
+	}
+
+	defer f.Close()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+
+	pb.RegisterLoggerServer(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
--- server/main.go
+++ server/main.go
@@ -1,10 +1,17 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/geoffrey-anto/golang-microservice-apis/protos"
 )
 
 type Server struct {
@@ -20,10 +27,35 @@ func NewServer(addr string, port int) *Server {
 }
 
 func (s *Server) RunServer() {
-	app := fiber.New()
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		conn, err := grpc.Dial(
+			"localhost:50051",
+			grpc.WithTransportCredentials(insecure.NewCredentials()),
+		)
+		if err != nil {
+			log.Fatalf("did not connect: %v", err)
+		}
+		defer conn.Close()
+		LoggerClient := pb.NewLoggerClient(conn)
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		r, err := LoggerClient.SaveLog(ctx, &pb.LogSaveRequest{
+			Id:   c.IP(),
+			Time: time.Now().String(),
+			Log:  "GET /",
+		})
+		if err != nil {
+			log.Fatalf("could not log: %v", err)
+		}
+		log.Printf("Log Success: %v", r.Success)
+
+		return c.Render("index", fiber.Map{})
 	})
 
 	app.Get("/health", func(c *fiber.Ctx) error {
